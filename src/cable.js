f4f77dce2757324b48801bac7cdff21defa7f3c8
Sergio D. Márquez <marquez.sergio.d@gmail.com>
2017-02-09 16:13:49 +1300

    Update dependencies, import bootstrap css from scss

--- src/cable.js
+++
@@ -1,6 +0,0 @@
- //= require action_cable
- //= require_self
- //= require_tree ./channels
-
- const App = {}
- App.cable = ActionCable.createConsumer()
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 // import { Navbar, Jumbotron, Button } from 'react-bootstrap'
 
 import TaskList from './components/task-list';
 
-import 'bootstrap/scss/bootstrap.scss'
 import 'font-awesome/scss/font-awesome.scss'
 import './scss/base.scss'
 
